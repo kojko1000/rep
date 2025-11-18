7de8c76f79715ef93921a677d7f1434e90fc8427
igorka5 <igorshulign2008@gmail.com>
2025-11-18 23:11:00 +0500

    fixed the door barking

--- zone-shope/scripts/dver.gd
+++ zone-shope/scripts/dver.gd
@@ -5,8 +5,9 @@ extends Sprite2D
 
 func _on_area_2d_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
 	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
-		sound.play()
-		await sound.finished
-		get_tree().change_scene_to_file("res://scense/street.tscn")
+		if !sound.playing:
+			sound.play()
+			await sound.finished
+			get_tree().change_scene_to_file("res://scense/street.tscn")
 		#to do !fix this shit or you will get fired!!!
 	pass # Replace with function body.
